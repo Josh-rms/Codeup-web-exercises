fb7243d614bac57f9b97582578c6860cfa050c43
Josh Ramos <josh.r.938@gmail.com>
2019-05-07 10:18:43 -0500

    This is the finished promise exercise V2.0

--- js/promises.js
+++ js/promises.js
@@ -28,3 +28,15 @@ const wait = (number) => {
 
 wait(1000).then(() => console.log("You\'ll see this after 1 second"));
 wait(3000).then(() => console.log("You\'ll see this after 3 seconds"));
+
+//--------------------------------------------------------------------------------------------------------------------//
+
+const commitInfo = (userName) => {
+    fetch(`https://api.github.com/users/${userName}/events`, {headers: {"Authorization": "gitHubKey"}})
+        .then(response => response.json())
+        .then((users) => {
+            alert(users[0].created_at);
+            console.log(users[0].created_at);
+        });
+};
+commitInfo("Josh-rms");
